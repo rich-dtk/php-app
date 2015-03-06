4b5a5948dc83ccb86495375f9d3ef7f6a4cf1f37
bjusufbe <bakir@atlantbh.com>
2015-03-06 13:13:01 +0100

    adding initial php script for testing mysql connectivity

--- test.php
+++ test.php
@@ -10,5 +10,6 @@ $conn = mysqli_connect($servername, $username, $password);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+echo "Connected successfully to MySQL database";
+phpinfo();
 ?>
